53f99c7b0c816f8ade40dd69537d59490486b562
Deepak Rangarajan <deepak.rangarajan@netl.doe.gov>
2023-05-02 15:41:09 +0000

    handle more cases

--- Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian_misc.cpp
+++ Src/LinearSolvers/MLMG/AMReX_MLNodeLaplacian_misc.cpp
@@ -1064,8 +1064,8 @@ MLNodeLaplacian::compRHS (const Vector<MultiFab*>& rhs, const Vector<MultiFab*>&
                             add_eb_flow_contrib_from_mismatched_faces(i,j,k,rhsarr,dmskarr,
                                 dxinvarr,mmintgxarr,eb_vel);
 
-                            if (i==19 && j==8 && k==4) {
-                                Print() << "rhs=" << rhsarr(i,j,k) << std::endl;
+                            if (std::abs(rhsarr(i,j,k)) > 1.e-1) {
+                                Print() << "ijk=" << IntVect(i,j,k) << ", rhs=" << rhsarr(i,j,k) << std::endl;
                             }
                         });
                         Abort();
