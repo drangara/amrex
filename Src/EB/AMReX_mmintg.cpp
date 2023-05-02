0e0bf0165ca9a3e50a77d2deae8cc41ee6b63fb6
Deepak Rangarajan <deepak.rangarajan@netl.doe.gov>
2023-05-02 22:52:42 +0000

    tweaks

--- Src/EB/AMReX_mmintg.cpp
+++ Src/EB/AMReX_mmintg.cpp
@@ -83,7 +83,7 @@ compute_mmintegral (const Array<std::unique_ptr<MultiFab>, AMREX_SPACEDIM> & mmi
                             for (int n = 0; n < numMmIntgs; ++n) mmintg(i,j,k,n) = 0.0;
                         } else {
                             if(idim == 0) {
-                                compute_mmintg_on_yz_face(i,j,k,mmintg,bc,bn);
+                                compute_mmintg_on_yz_face(i,j,k,mmintg,bc,bn,fg);
                             }
                         }
                     }
